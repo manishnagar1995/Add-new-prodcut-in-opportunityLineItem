27b55363ebd9e6959c559e9e38b3118d5ad3e0ab
manish nagar <33551293+manishnagar1995@users.noreply.github.com>
2020-01-18 15:13:33 +0530

    final working

--- Add OLI/force-app/main/default/lwc/add_OpportunityLineItem/add_OpportunityLineItem.js
+++ Add OLI/force-app/main/default/lwc/add_OpportunityLineItem/add_OpportunityLineItem.js
@@ -1,40 +1,56 @@
+/* eslint-disable eqeqeq */
+/* eslint-disable no-debugger */
+/* eslint-disable guard-for-in */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
 /* eslint-disable no-alert */
 /* eslint-disable no-restricted-globals */
 /* eslint-disable no-console */
-import { LightningElement,track,api } from 'lwc';
-import insertOlI from '@salesforce/apex/Add_OpportunityLientItem.insertOLI';
+import { LightningElement,track,api,wire } from 'lwc';
+import searchImperativeContactList from '@salesforce/apex/Add_OpportunityLientItem.insertOLI';
 
 export default class Add_OpportunityLineItem extends LightningElement {
     @track openModal=false;
-   // @track objectapiname;
+    @track objectapiname;
     @api recordId;
+    @track error;
     @track prodName;
-
+    @track pbentry;
+  
     handleClick(){
         this.openModal=true;
         console.log('Test========',   this.openModal);
     }
 
     handleSubmit(event){
-        event.preventDefault();
-         // this.prodName =event.detail.fields;
-          const fields = event.detail.fields;
-        console.log('prodName=====',fields.product_type__c);
-        console.log('prodName=====',fields);
-        console.log('Oppportunity_id=====',this.recordId);
-       insertOlI({prodName:'$prodName'});
-
-     /* //event.preventDefault();
-        this.validForm = true;
-        // check for validations and set validForm
-        if (this.validForm) {
-            //this.template.querySelector('lightning-record-edit-form').submit();
-           // this.openModal=false;
-        } else {
-            this.showToast();
-        }*/
+       // debugger;
         
-    }
+    
+          const fields = event.detail.fields;
+          let oppid =this.recordId;
+          fields.OpportunityId = oppid;
+          //let pbentry;
+          event.preventDefault();
+        console.log('prodName=====',fields.product_type__c);
+        console.log('Oppportunity_id=====',oppid);
+        searchImperativeContactList({ prodNames : fields.product_type__c,oppids: oppid })
+        .then(result => {
+            console.log('result == ', JSON.stringify(result));
+            this.pbentry = result.Id;
+            console.log('id23=======',this.pbentry); 
+            fields.PricebookEntryId =this.pbentry;
+            
+            console.log('isnert======',JSON.stringify(fields));
+            
+            this.template.querySelector('lightning-record-edit-form').submit(fields);  
+            this.openModal=false;
+
+        })
+        .catch(error => {
+            this.error = error;
+            this.pbentry = undefined;
+        });
+}
 
     closeModal(){
         this.openModal=false;
